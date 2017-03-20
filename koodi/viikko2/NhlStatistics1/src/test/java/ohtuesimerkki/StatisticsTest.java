d75c0ea651917c58c6f581c0076dd5b65ae88c3f
MJL7068 <mattil@netti.fi>
2017-03-20 16:35:41 +0200

    tehty junit-tehtävä

---
+++ koodi/viikko2/NhlStatistics1/src/test/java/ohtuesimerkki/StatisticsTest.java
@@ -0,0 +1,58 @@
+package ohtuesimerkki;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class StatisticsTest {
+ 
+    Reader readerStub = new Reader() {
+ 
+        public List<Player> getPlayers() {
+            ArrayList<Player> players = new ArrayList<Player>();
+ 
+            players.add(new Player("Semenko", "EDM", 4, 12));
+            players.add(new Player("Lemieux", "PIT", 45, 54));
+            players.add(new Player("Kurri",   "EDM", 37, 53));
+            players.add(new Player("Yzerman", "DET", 42, 56));
+            players.add(new Player("Gretzky", "EDM", 35, 89));
+ 
+            return players;
+        }
+    };
+ 
+    Statistics stats;
+
+    @Before
+    public void setUp(){
+        // luodaan Staatistics-olio joka käyttää "stubia"
+        stats = new Statistics(readerStub);
+    }  
+    
+    @Test
+    public void hakuToimii() {
+        assertEquals("Semenko", stats.search("Semenko").getName());
+    }
+    
+    @Test
+    public void hakuPalauttaaNull() {
+        assertEquals(null, stats.search("qwerty"));
+    }
+    
+    @Test
+    public void palauttaaOikeanJoukkueenKoon() {
+        assertEquals(3, stats.team("EDM").size());
+    }
+    
+    @Test
+    public void palauttaaOikeanPistemaaran() {
+        assertEquals(124, stats.topScorers(1).get(0).getPoints());
+    }
+}
+
+
